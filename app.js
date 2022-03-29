7dd243c757d1b7314562256ca582dcce5abc129b
Thomas Werthman <tommygun3151224@gmail.com>
2022-03-29 06:59:00 -0400

    Made game function to play 5 rounds

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
- 
-        let playerSelection;
-        let playerScore = 0
-        let computerScore = 0
-        let computerSelection;
+let playerSelection;
+let playerScore = 0
+let computerScore = 0
+let computerSelection;
 
-        const moves = ["Rock", "Paper", "Scissors"]
+const moves = ["Rock", "Paper", "Scissors"]
 
-        function computerPlay() {
-            return moves[(Math.random() * moves.length) | 0]
+function computerPlay() {
+    return moves[(Math.random() * moves.length) | 0]
             
             
-        }
+     }
 
-        function play(playerSelection = prompt("Rock, Paper, or Scissors?"), computerSelection) {
-            computerSelection = computerPlay().toLowerCase()
-            playerSelection = playerSelection.toLowerCase()
+    function play(playerSelection = prompt("Rock, Paper, or Scissors?"), computerSelection) {
+         computerSelection = computerPlay().toLowerCase()
+         playerSelection = playerSelection.toLowerCase()
             
-            if(computerSelection === playerSelection){
+         if(computerSelection === playerSelection){
             console.log("Tie!!");
 
-            }else if(
-                (computerSelection == 'rock' && playerSelection == 'scissors') ||
-                (computerSelection == 'scissors' && playerSelection == 'paper') ||
-                (computerSelection == 'paper' && playerSelection == 'rock')
-                ){
-                computerScore = ++computerScore;
-                console.log('Computer scored!!!')
+         }else if(
+            (computerSelection == 'rock' && playerSelection == 'scissors') ||
+            (computerSelection == 'scissors' && playerSelection == 'paper') ||
+            (computerSelection == 'paper' && playerSelection == 'rock')
+             ){
+             computerScore = ++computerScore;
+             console.log('Computer scored!!!')
 
-                }else{
-                    playerScore = ++playerScore;
-                    console.log('You scored!!')
+            }else{
+                 playerScore = ++playerScore;
+                 console.log('You scored!!')
                 }       
         } 
+
+function game(){
+    for (let i = 0; i < 5 ; i++){
         play(playerSelection, computerSelection)
+    }
+}
+
+game()
+        
